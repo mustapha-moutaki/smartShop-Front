35082bc637a9f2506bfcbbcb79cbea5f2819bb76
mustapha <mustaphaamoutaki@gmail.com>
2026-02-02 16:47:57 +0100

    complete order service

---
+++ src/services/order.api.ts
@@ -0,0 +1,36 @@
+import apiClient from "../../lib/ApiClient"
+import type { CreateOrderPayload } from "../types/order";
+// Base
+const ORDER_BASE = "/orders"
+
+
+// get all orders
+export const getOrders = async ()=>{
+    const response = await apiClient.get(ORDER_BASE);
+    return response.data
+}
+
+// get by id
+export const getOrderById = async(id: number) =>{
+    const response = await apiClient.get(`${ORDER_BASE}/${id}`);
+    return response.data;
+}
+
+
+// create order
+export const createOrder = async(payload: CreateOrderPayload)=>{
+    const response =  await apiClient.post(ORDER_BASE, payload);
+    return response.data;
+}
+
+// update order
+export const updateOrder =  async(id: number, payload: any)=>{
+    const response =  await apiClient.put(`${ORDER_BASE}/${id}`, payload);
+    return response.data;
+}
+
+// delete order
+export const deleteOrder = async(id: number)=>{
+    const response = await apiClient.delete(`${ORDER_BASE}/${id}`);
+    return response.data
+}
