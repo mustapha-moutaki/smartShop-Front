2240a7417fe18930f6092dc50eb9e40e26dc1b42
mustapha <mustaphaamoutaki@gmail.com>
2026-02-03 02:34:00 +0100

    feat: display user data in the header

---
+++ src/hooks/AuthContext.ts
@@ -0,0 +1,52 @@
+// import React, { createContext, useContext, useState, useEffect } from "react";
+
+// // 1. Define what the User looks like
+// interface User {
+//   id: number;
+//   username: string;
+//   role: string;
+// }
+
+// interface AuthContextType {
+//   user: User | null;
+//   login: (userData: User) => void;
+//   logout: () => void;
+// }
+
+// const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+// export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [user, setUser] = useState<User | null>(null);
+
+//   // 2. On Load: Check if user is already in localStorage
+//   useEffect(() => {
+//     const savedUser = localStorage.getItem("user");
+//     if (savedUser) {
+//       setUser(JSON.parse(savedUser));
+//     }
+//   }, []);
+
+//   const login = (userData: User) => {
+//     setUser(userData);
+//     localStorage.setItem("user", JSON.stringify(userData));
+//   };
+
+//   const logout = () => {
+//     setUser(null);
+//     localStorage.removeItem("user");
+//     window.location.href = "/login";
+//   };
+
+//   return (
+//     <AuthContext.Provider value={{ user, login, logout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// // Custom hook for easy access
+// export const useAuth = () => {
+//   const context = useContext(AuthContext);
+//   if (!context) throw new Error("useAuth must be used within AuthProvider");
+//   return context;
+// };
