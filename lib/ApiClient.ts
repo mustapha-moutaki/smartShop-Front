4c16feee7e6e9d39760338bbf17eb0ae86ccd308
mustapha <mustaphaamoutaki@gmail.com>
2026-02-03 14:12:51 +0100

    feat: add middleware axios interceptor to handle expired session

--- lib/ApiClient.ts
+++ lib/ApiClient.ts
@@ -10,6 +10,28 @@ const apiClient = axios.create({
   },
 });
 
+
+
+/// response interceptor to handle Session expiration and redirect user to login
+apiClient.interceptors.response.use(
+  (response)=>{
+    // if the request is successfully
+    return response;
+  },
+  (error)=>{
+    //  chekc the error type if it's anuthorized 401
+    if(error.response && error.response.status === 401){
+      console.warn("Session expired or unauthorized. Redirecting to login ...");
+
+      localStorage.removeItem("user");
+
+      window.location.href='/login'
+    }
+
+    return Promise.reject(error)
+  }
+);
+
 // if we use  JWT interceptor 
 // apiClient.interceptors.request.use(config => {
 //   const token = localStorage.getItem("token");
