c2c3f6ab2566e63121e39556c75b184fb84e77b9
mustapha <mustaphaamoutaki@gmail.com>
2026-02-05 13:56:56 +0100

    add the routes

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,8 @@ import OrderShowPage from "./pages/Orders/OrderShowPage.tsx";
 import PromocodePage from "./pages/Promocode/PromocodePage.tsx";
 import EditPromocodePage from "./pages/Promocode/EditPromocodePage.tsx";
 import CreatePromocodePage from "./pages/Promocode/commun/CreatePromocodePage.tsx";
-import CreateOrderPage from ".//pages/Orders/CreatOrderPage.tsx"
+import ProductsPage from "./pages/Products/ProductsPage.tsx";
+// import CreateOrderPage from ".//pages/Orders/CreatOrderPage.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -34,7 +35,9 @@ createRoot(document.getElementById("root")!).render(
               <Route  path="/orders" element={<OrdersPage/>} />
               <Route path="/admin-dashboard" element={< DashboardPage/>} />
               <Route path="/orders/:id" element={< OrderShowPage/>} />
-              <Route path="/orders/new" element={< CreateOrderPage/>} />
+              {/* <Route path="/orders/new" element={< CreateOrderPage/>} /> */}
+
+              <Route path="/products" element={<ProductsPage/> } />
 
               <Route path="/promocodes" element={< PromocodePage/>} />
               <Route path="/promocodes/:id" element={< EditPromocodePage/>} />
