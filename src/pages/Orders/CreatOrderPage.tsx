ae258c5e1b081075ce5f511aecfdd2670eddd446
mustapha <mustaphaamoutaki@gmail.com>
2026-02-05 13:57:29 +0100

    create page or order

--- src/pages/Orders/CreatOrderPage.tsx
+++ src/pages/Orders/CreatOrderPage.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import OrderForm from "./components/OrderFom";
+// import { useState } from "react";
+// import OrderForm from "./components/OrderFom";
 
-const CreateOrderPage = ()=>{
+// const CreateOrderPage = ()=>{
 
-    const [promocodes, setPromocodes] = useState<any[]>([])
-    const [clients, setClients] = useState<any[]>([])
-    const [items, setItems] = useState<any[]>([])
-    return(
-        <>
-        <div>
-            <h1>Create order</h1>
-            <OrderForm promocodes={promocodes} clients={clients} items={items} />
-        </div>
-        </>
-    )
-};
-export default CreateOrderPage;
+//     const [promocodes, setPromocodes] = useState<any[]>([])
+//     const [clients, setClients] = useState<any[]>([])
+//     const [items, setItems] = useState<any[]>([])
+//     return(
+//         <>
+//         <div>
+//             <h1>Create order</h1>
+//             <OrderForm promocodes={promocodes} clients={clients} items={items} />
+//         </div>
+//         </>
+//     )
+// };
+// export default CreateOrderPage;
