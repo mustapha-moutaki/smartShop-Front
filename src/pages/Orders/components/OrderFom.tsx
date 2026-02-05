4dc8b8bdcbd60bdf0463b73667a3681b4e5e7a22
mustapha <mustaphaamoutaki@gmail.com>
2026-02-05 13:57:48 +0100

    create order form

--- src/pages/Orders/components/OrderFom.tsx
+++ src/pages/Orders/components/OrderFom.tsx
@@ -1,38 +1,38 @@
-const OrderForm = ({ promocodes, clients, items }) => {
-  return (
-    <div>
-      <form>
+// const OrderForm = ({ promocodes, clients, items }) => {
+//   return (
+//     <div>
+//       <form>
 
-        <label>choose the client</label>
-        <select>
-          {clients.map((client) => (
-            <option key={client.id} value={client.id}>
-              {client.name}
-            </option>
-          ))}
-        </select>
+//         <label>choose the client</label>
+//         <select>
+//           {clients.map((client) => (
+//             <option key={client.id} value={client.id}>
+//               {client.name}
+//             </option>
+//           ))}
+//         </select>
       
 
-        <label>promocode</label>
-        <select>
-          {promocodes.map((promo) => (
-            <option key={promo.id} value={promo.id}>
-              {promo.code}
-            </option>
-          ))}
-        </select>
+//         <label>promocode</label>
+//         <select>
+//           {promocodes.map((promo) => (
+//             <option key={promo.id} value={promo.id}>
+//               {promo.code}
+//             </option>
+//           ))}
+//         </select>
 
-        <label>items</label>
-        {items.map((item) => (
-          <div key={item.id}>
-            <input type="checkbox" value={item.id} />
-            <span>{item.name}</span>
-          </div>
-        ))}
+//         <label>items</label>
+//         {items.map((item) => (
+//           <div key={item.id}>
+//             <input type="checkbox" value={item.id} />
+//             <span>{item.name}</span>
+//           </div>
+//         ))}
 
-      </form>
-    </div>
-  );
-};
+//       </form>
+//     </div>
+//   );
+// };
 
-export default OrderForm;
+// export default OrderForm;
